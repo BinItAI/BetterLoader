866c835d04fff2bb778f8d0bd6769badf081d23d
Raghav Mecheri <raghav.mecheri@columbia.edu>
2020-08-08 15:08:17 +0530

    Cleaned up code + added samples/

--- betterloader/__init__.py
+++ betterloader/__init__.py
@@ -4,7 +4,7 @@ BetterLoader
 <strikethrough>Fixing the Python DataLoader :)</strikethrough>
 
 Adding Hypercustomizability to the Pytorch ImageFolder Dataloader
-id est: making it harder to do easy things, but easier to do harder things
+id est: making it harder to do easy things, but easier to do harder things :)
 
 """
 
@@ -27,7 +27,6 @@ else:
 def check_valid(subset_json):
     if subset_json == None:
         return lambda path: True
-        #print('No subset json specified, using entire dataset...')
 
     def curry(image_path):
         if image_path in subset_json:
@@ -36,53 +35,40 @@ def check_valid(subset_json):
     return curry
 
 class BetterLoader:
-	def __init__(self):
-		pass
+	def __init__(self, basepath, index_json_path, num_workers=1, subset_json_path=None):
+		if not os.path.exists(basepath):
+			raise Exception("Please supply a valid path to your base folder!")
 
-	def fetch_segmented_dataloaders(self, basepath, batch_size, transform, num_workers=8, subset_json_path=None, index_json_path=None, dataset_metadata = None):
+		if not os.path.exists(index_json_path):
+			raise Exception("Please supply a valid path to a dataset index file!")
+
+		self.basepath = basepath
+		self.num_workers = num_workers
+		self.subset_json_path = subset_json_path
+		self.index_json_path = index_json_path
+
+	def fetch_segmented_dataloaders(self, batch_size, transform, dataset_metadata = None):
 		'''Return a 2 element tuple, containing a list of dataloaders (train, test, split) along with a tuple containing their sizes'''
 		
-		#when we use this we can just have a separate file that defines this metadata per dataset, as it should always be constant
-		#we might want to add the index_json to the metadata
-		 train_test_val_instances, class_data, pretransform = dataset_metadata
+		train_test_val_instances, class_data, pretransform = dataset_metadata
 
+		index, subset_json = fetch_json_from_path(self.index_json_path), fetch_json_from_path(self.subset_json_path)
 
-		#this code feels unnecessary rn, we should just enforce that the user supplies the index.
-		#or we can put the index in the metadata
-		'''if index_json_path == None:
-        	print("No index path specified, using default index path...")
-			_ensure_folder(DEFAULT_INDEX_PATH)
-			cached_file = os.path.join(DEFAULT_INDEX_PATH, DEFAULT_INDEX)
-			if os.path.exists(cached_file) and os.path.isfile(cached_file):
-				index_json_path = cached_file
-			else:
-				_download_default_index_file(cached_file)
-				index_json_path = cached_file
-		else:
-			print("Specified index json path, using the same...")
-		'''
-
-		subset_json = fetch_json_from_path(subset_json_path)
-
-		#i have absolutely no idea why this code exists
-		#subset_json = None if subset_json == None else set(subset_json)
-
-		index = fetch_json_from_path(index_json_path)
-
-		datasets = [ImageFolderCustom(root=basepath, transform=transform, is_valid_file=check_valid(
+		datasets = [ImageFolderCustom(root=self.basepath, transform=transform, is_valid_file=check_valid(
 			subset_json), instance=x, index=index,  train_test_val_instances=train_test_val_instances, class_data=class_data, pretransform = pretransform) for x in ('train', 'test', 'val')]
-		dataloaders = [torch.utils.data.DataLoader(x, batch_size=batch_size, num_workers=num_workers, shuffle=True) for x in [
-			datasets[0]]] + [torch.utils.data.DataLoader(x, batch_size=batch_size, num_workers=num_workers, shuffle=False) for x in datasets[1:]]
+		dataloaders = [torch.utils.data.DataLoader(x, batch_size=batch_size, num_workers=self.num_workers, shuffle=True) for x in [
+			datasets[0]]] + [torch.utils.data.DataLoader(x, batch_size=batch_size, num_workers=self.num_workers, shuffle=False) for x in datasets[1:]]
+		
 		loaders = {
 			"train": dataloaders[0],
 			"test": dataloaders[1],
 			"val": dataloaders[2]
 		}
+
 		sizes = {
 			"train": len(datasets[0]),
 			"test": len(datasets[1]),
 			"val": len(datasets[2])
 		}
-		print(str(sizes))
+		
 		return loaders, sizes
-			return None, None
