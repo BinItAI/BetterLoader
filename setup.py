1ef87bba664de228a7f999778516b924ad65aaf3
Raghav Mecheri <raghav.mecheri@columbia.edu>
2020-08-11 15:19:40 +0530

    Added long_description

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='BetterLoader',
     version='0.1.0',    
     description='A better PyTorch dataloader',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/BinItAI/BetterLoader',
     author='BinIt Inc',
     author_email='',
