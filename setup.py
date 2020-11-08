cb270d4117d5f0aace4415995b34a5c12c772639
Raghav Mecheri <raghav.mecheri@columbia.edu>
2020-11-08 20:00:49 +0530

    Version bump to 0.1.7

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='BetterLoader',
-    version='0.1.6',    
+    version='0.1.7',    
     description='A better PyTorch dataloader',
     long_description=long_description,
     long_description_content_type='text/markdown',
